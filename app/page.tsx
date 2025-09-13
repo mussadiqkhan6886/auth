b92592ee966b34a1dc3a93c0c12ff9a9e53578f9
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-13 18:01:42 +0500

    design home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/login">Login</Link>
-      <Link href="/signup">Signup</Link>
+    <div className="flex justify-center items-center bg-gray-100 gap-10 h-screen">
+      <Link className="text-4xl" href="/login">Login</Link>
+      <Link className="text-4xl" href="/signup">Signup</Link>
     </div>
   );
 }
