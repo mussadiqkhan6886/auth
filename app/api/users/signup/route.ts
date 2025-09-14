f975c994e4b1cd37b681a1c6c5fcb0e438a81989
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-14 15:11:38 +0500

    started signup page route

---
+++ app/api/users/signup/route.ts
@@ -0,0 +1,7 @@
+import { connectDB } from "@/lib/config/database";
+import User from "@/lib/model/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs"
+
+await connectDB()
+
---
+++ lib/model/userModel.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, "Please Provide Username"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please Provide Email"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please Provide Password"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpire: Date
+})
+
+const User = mongoose.models.users || mongoose.model("users", userSchema)
+
+export default User
